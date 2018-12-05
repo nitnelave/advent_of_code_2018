5abbee207ef01581f1d2a6c38d1701731fbe5fb2
Valentin Tolmer <valentin.tolmer@gmail.com>
2018-12-05 18:03:57 +0100

    day_01: "Improve" day 1's python solution into a 1_liner

--- 01/solution_2.py
+++ 01/solution_2.py
@@ -1,10 +1,1 @@
-import itertools
-def find_repeated_frequency():
-    sum = 0
-    seen = set()
-    for n in itertools.cycle(map(int, open('input'))):
-        if sum in seen:
-            return sum
-        seen.add(sum)
-        sum += n
-print(find_repeated_frequency())
+print((lambda s=set(): next(i for i, st, _ in ([i, s.copy(), s.add(i)] for i in __import__('itertools').accumulate(__import__('itertools').cycle(map(int, open('input'))))) if i in st))())
